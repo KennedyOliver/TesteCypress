959bbbb7a4f45bca31d1ba1b7647b20e2f5f69ed
KennedyOliver <kennedy.oliver90@gmail.com>
2026-03-02 19:47:28 -0300

    Att13

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     video: true,
-    videoCompression: 32,
+    videoCompression: false,
     videoUploadOnPasses: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here, se precisar
